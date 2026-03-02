5016d836a905ab677734956baf520547c62bf105
Fl1ckerxD <mihaylav.slava@yandex.ru>
2026-03-02 20:13:17 +0300

    Delete unnecessary files

--- MyForum.Api/Web/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MyForum.Api.Web.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
